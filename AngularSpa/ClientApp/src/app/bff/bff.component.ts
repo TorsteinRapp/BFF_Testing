0505021d11a1db7e46e8ffd1da2841b1bfedf0ad
Petter Hoel <petter@pettehoel.net>
2020-01-24 18:46:41 +0100

    adds bff-component and defaults routing to it

---
+++ AngularSpa/ClientApp/src/app/bff/bff.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bff',
+  templateUrl: './bff.component.html',
+})
+export class BffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
