33192c2c49e74bbbcbed0958273067f2f6893f5e
Petter Hoel <petter@pettehoel.net>
2020-01-26 10:50:56 +0100

    spa henter og viser frem data tilsvarende Host-prosjektet

--- AngularSpa/ClientApp/src/app/bff/bff.component.ts
+++ AngularSpa/ClientApp/src/app/bff/bff.component.ts
@@ -1,14 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-bff',
   templateUrl: './bff.component.html',
 })
-export class BffComponent implements OnInit {
+export class BffComponent {
+  data;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  getFrontend(): void {
+    const url = 'user/info';
+    this.http
+      .get<any>(url)
+      .subscribe(this.setData, this.handleError);
   }
 
+  getBackend(): void {
+    const url = 'user/infoFromExternal';
+    this.http
+      .get<any>(url)
+      .subscribe(this.setData, this.handleError);
+  }
+
+  setData = data => {
+    this.data = data;
+  }
+
+  handleError = error => {
+    console.info('Something went wrong');
+    console.error(error);
+  }
 }
